9b864debcb1c80f8900360b5c1499af98e9b24a0
Nurul Izzah <nurulizzah.pjs@gmail.com>
2024-11-18 13:29:49 +0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,86 @@
+(function() {
+  "use strict";
+
+  /**
+   * Apply .scrolled class to the body as the page is scrolled down
+   */
+  function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeader = document.querySelector('#header');
+    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  }
+
+  document.addEventListener('scroll', toggleScrolled);
+  window.addEventListener('load', toggleScrolled);
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
+   * Countdown timer
+   */
+  function updateCountDown(countDownItem) {
+    const timeleft = new Date(countDownItem.getAttribute('data-count')).getTime() - new Date().getTime();
+
+    const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    countDownItem.querySelector('.count-days').innerHTML = days;
+    countDownItem.querySelector('.count-hours').innerHTML = hours;
+    countDownItem.querySelector('.count-minutes').innerHTML = minutes;
+    countDownItem.querySelector('.count-seconds').innerHTML = seconds;
+
+  }
+
+  document.querySelectorAll('.countdown').forEach(function(countDownItem) {
+    updateCountDown(countDownItem);
+    setInterval(function() {
+      updateCountDown(countDownItem);
+    }, 1000);
+  });
+
+})();
